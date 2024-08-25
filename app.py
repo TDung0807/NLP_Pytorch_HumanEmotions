4c7400c63f153879903289266c63eff7ce1e8027
Trdung0807 <trungdungg0807@gmail.com>
2024-08-25 15:14:23 +0700

    init app.py

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 import pandas as pd
 import torch
 import torch.nn as nn
+df_train= pd.read_csv("./data/train.csv")
+df_test = pd.read_csv("./data/test.csv")
+df_val = pd.read_csv("./data/val.csv")
+
+
+print(df_train.head())
