2b4f45defa6683a75e6730c0c204553513b63a7c
Trdung0807 <trungdungg0807@gmail.com>
2024-08-25 15:38:43 +0700

    DataProcessing: remove stop words

--- DataProcessing.py
+++ DataProcessing.py
@@ -7,8 +7,17 @@ class DataProcessing:
         self.data = data
     def tokenizedWord(self,row):
         self.data["tokenized"] = self.data[row].apply(lambda x: re.findall(r'\b\w+\b',x.lower()))
+        self.stopWord = pd.read_csv('stopword.csv')
+        arr = self.stopWord.transpose().values
+        self.data["tokenized"] = self.data['tokenized'].apply(lambda x: self.remove_stopWord(x,arr))
         print(self.data.head())
-
+        
+    def remove_stopWord(self,dataRow,stopWords):
+        for i in dataRow:
+            if i in stopWords:
+                dataRow.remove(i)
+        return dataRow
+    
 # Example usage
 df = pd.read_csv("./data/train.csv")
 dp = DataProcessing(df)
