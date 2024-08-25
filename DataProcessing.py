9ec1477e3a6ab62982dc7af2d95b78088d0dc8b3
Trdung0807 <trungdungg0807@gmail.com>
2024-08-25 16:21:49 +0700

    DataProcessing: find frequency

--- DataProcessing.py
+++ DataProcessing.py
@@ -2,23 +2,35 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import re
+from sklearn.feature_extraction.text import HashingVectorizer
+
 class DataProcessing:
     def __init__(self, data):
         self.data = data
+        
     def tokenizedWord(self,row):
         self.data["tokenized"] = self.data[row].apply(lambda x: re.findall(r'\b\w+\b',x.lower()))
         self.stopWord = pd.read_csv('stopword.csv')
         arr = self.stopWord.transpose().values
         self.data["tokenized"] = self.data['tokenized'].apply(lambda x: self.remove_stopWord(x,arr))
-        print(self.data.head())
-        
-    def remove_stopWord(self,dataRow,stopWords):
-        for i in dataRow:
-            if i in stopWords:
-                dataRow.remove(i)
-        return dataRow
+        pass
     
+    def wordFrequency(self):
+        vectorizer = HashingVectorizer(n_features=6)
+        self.data["text"] = self.data["tokenized"].apply(lambda x: ' '.join(x))
+        vector = vectorizer.transform(self.data['text'])
+        self.data["frequency"] = list(vector.toarray())
+        print(self.data)
+        pass
+    
+    def remove_stopWord(self, dataRow, stopWords):
+        return [word for word in dataRow if word not in stopWords]
+    
+    def main(self,row):
+        self.tokenizedWord(row)
+        self.wordFrequency()
+        pass
 # Example usage
 df = pd.read_csv("./data/train.csv")
 dp = DataProcessing(df)
-dp.tokenizedWord('message')
+dp.main('message')
