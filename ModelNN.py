13aae6d5bb9161d05bcdb2cd7c8af2cf1073deb9
Trdung0807 <trungdungg0807@gmail.com>
2024-08-26 17:39:35 +0700

    create simple Neutral Network model

---
+++ ModelNN.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class ModelNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(ModelNN, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return self.softmax(x)
