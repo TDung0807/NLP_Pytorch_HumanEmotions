d5c0b8199cc21e0a9fa96c4a336470ad8b2d3fca
Trdung0807 <trungdungg0807@gmail.com>
2024-08-26 17:36:53 +0700

    convert emotion to label

--- app.py
+++ app.py
@@ -12,8 +12,12 @@ df_val = pd.read_csv("./data/val.csv")
 process = DataProcessing(df_train)
 df_train = process.main('message')
 
-print(df_train.head())
 
 X = df_train['frequency'].tolist()
 y = df_train['emotion'].tolist()  
+unique_labels = list(set(y))
+label_to_index = {label: idx for idx, label in enumerate(unique_labels)}
+
+df_train['label'] = df_train['emotion'].map(label_to_index)
+
 
