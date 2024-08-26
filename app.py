6593fb7b95bde01902577b7a2eecca61523785a7
Trdung0807 <trungdungg0807@gmail.com>
2024-08-26 17:45:59 +0700

    implement ModelNN

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # main.py
 import pandas as pd
 import torch
+import torch.nn as nn
 from sklearn.model_selection import train_test_split
 from DataProcessing import DataProcessing  # Import the DataProcessing class
+from torch.utils.data import DataLoader, TensorDataset
+from ModelNN import ModelNN 
 
 # Load your data
 df_train = pd.read_csv("./data/train.csv")
@@ -21,3 +24,19 @@ label_to_index = {label: idx for idx, label in enumerate(unique_labels)}
 df_train['label'] = df_train['emotion'].map(label_to_index)
 
 
+y = df_train['label'].tolist()
+
+X_tensors = torch.tensor(X)
+y_tensors = torch.tensor(y)
+
+dataset = TensorDataset(X_tensors, y_tensors)
+train_loader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+# Define model parameters
+input_size = len(X_tensors[0])  # Number of features
+hidden_size = 64  # Size of the hidden layer
+output_size = len(set(y))  # Number of classes
+
+model = ModelNN(input_size, hidden_size, output_size)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
