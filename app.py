60a33b1e0caa97d9b678fff35085d283e82f3bb4
Trdung0807 <trungdungg0807@gmail.com>
2024-08-26 17:27:40 +0700

    implement + using DataProcessing class

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+# main.py
 import pandas as pd
 import torch
-import torch.nn as nn
-df_train= pd.read_csv("./data/train.csv")
+from sklearn.model_selection import train_test_split
+from DataProcessing import DataProcessing  # Import the DataProcessing class
+
+# Load your data
+df_train = pd.read_csv("./data/train.csv")
 df_test = pd.read_csv("./data/test.csv")
 df_val = pd.read_csv("./data/val.csv")
 
+process = DataProcessing(df_train)
+df_train = process.main('message')
 
 print(df_train.head())
+
+X = df_train['frequency'].tolist()
+y = df_train['emotion'].tolist()  
+
